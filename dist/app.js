3fa2ee57822e0318bc783ff2a85308134a563b2e
Weikai Zhang <weikai@gmail.com>
2020-04-10 22:36:23 -0400

    Added nara logo

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,10 @@
 // import your web components that make up
 // your app / cdn / unbundled deployment
 // import "@lrnwebcomponents/video-player/video-player.js";
+
+
+// common in CMSs
+import "@lrnwebcomponents/cms-hax/cms-hax.js";
+import "@lrnwebcomponents/wysiwyg-hax/wysiwyg-hax.js";
+
+import "nara-logo/nara-logo.js";
